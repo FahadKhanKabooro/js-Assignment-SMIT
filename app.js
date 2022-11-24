463060914b5e06d0f634becdc708521cf6691413
unknown <fahadkabooro.fk@gmail.com>
2022-11-24 21:20:53 +0500

    Assignment chapter 17 copleted

--- app.js
+++ app.js
@@ -1097,22 +1097,87 @@
         //         document.write("/select")
 
 
-// Assignmen CHapter 16 Completed 
+// Assignmen CHapter 16 Completed
 
+// Assignment CHapter of 17-20 STARTED
 
+// 1. Declare and initialize an empty multidimensional array. (Array of arrays)
+        // var array = [
+        //         [],[],[]
+        // ]
 
+// 2. Declare and initialize a multidimensional array representing the following matrix:
+        // var array = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
 
+// 3. Write a program to print numeric counting from 1 to 10.
+        // var array =[1,2,3,4,5,6,7,8,9,10,11] 
+        // for (i = 1;i <array.length;i++){
+        //         document.write("<br>"+i)
+        // }
 
+// 4. Write a program to print multiplication table of any number using for loop.
+//  Table number & length should be taken as an input from user.
 
+        // var table = prompt("Enter a Table number")
+        // var time = prompt("Enter the Table Lenght")
+        // for(let i =1; i <=time; i++){
+                
+        //         document.write(table + "*" +i+ "=" +(i*table)+"<br/>");
+                
 
+        // }
 
-
-
-
-
-
-
-
-
-
+// 5. Write a program to print items of the 
+// following array using for loop: fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+        // var fruits = ["apple ", " banana", " mango", " orange", " strawberry"]
+        // for(i = 0;i < fruits.length;i++){
+        //         document.write(fruits[i]+"<br>")       
+        // }
         
+        // var fruits = ["apple ", " banana", " mango", " orange", " strawberry"]
+        // for(i = 0;i < fruits.length;i++){
+               
+        //         document.write("<br>"+"Element at index of " +i+ " is "+(fruits[i]))
+                
+        // }
+        
+// 6. Generate the following series in your browser. See example output. 
+
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 
+        
+// var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        // for( i = 0;i < counting.length;i++){
+        //         document.write(counting[i])
+        // }
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 
+
+//  var reverse = [10 , 9 , 8 , 7 , 6 , 5 , 4 , 3 , 2 ,1]
+//  for(i = 0;i < reverse.length;i++){
+//         document.write(reverse[i])
+//  }
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 
+        
+// var even = [0 , 2 , 4 , 6 , 8 , 10 , 12 , 14 , 16 , 18 , 20]
+        // for(i = 0;i < even.length;i++){
+        //         document.write(even[i])
+        // }
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 
+
+//  var odd = [1,3,5,7,9,11,13,15,17,19]
+//  for(i = 0;i < odd.length;i++){
+//         document.write(odd[i])
+//  }
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+        // var series = ["2k","4k","6k","8k","10","12k","14k","16k","18k","20"]
+        // for(i = 0;i < series.length;i++){
+        //         document.write(series[i])
+        // /}
+
+// 7. You have an array 
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”] 
+// Write a program to enable “search by user input” in an array. After searching,
+//  prompt the user whether the given item is found in the list or not. Example:
+
+
+
